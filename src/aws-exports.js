ad1da83fcfc9bf145266de980951e0214811886b
eddy <caijinshu@tanguo.co>
2026-03-11 17:49:09 +0800

    Sync to Public Repository

---
+++ src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  Auth: {
+    region: 'ap-northeast-1',
+    userPoolId: 'ap-northeast-1_bY2byhlCa',
+    userPoolWebClientId: '6ialca0p8u0lsgvbmvsljfm305',
+    identityPoolId: 'ap-northeast-1:0a1820f1-dbb3-4bca-9227-2a92f6abf0ae',
+    mandatorySignIn: false,
+  },
+};
+export default awsmobile;
---
+++ src/env_config.js
@@ -0,0 +1,12 @@
+const getConfigAwsUrl = () => {
+  return 'wss://82q6nuplv0.execute-api.ap-northeast-1.amazonaws.com/public';
+};
+
+const getConfigStripKey = () => {
+  return 'pk_live_SAO5zmCNp1HfQxRkvfLU7YwS';
+};
+
+export const envConfig = {
+  getConfigAwsUrl,
+  getConfigStripKey,
+};
